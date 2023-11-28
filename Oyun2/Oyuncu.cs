862477b8d3f1c10a5d01d2ee51ce7b668c07b12b
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-28 11:20:59 +0300

    Add files via upload

---
+++ Oyun2/Oyuncu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oyun2
+{
+    public class Oyuncu
+    {
+        public string Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Posta { get; set; }
+        public double Puan { get; set; }
+        public DateTime KayitTarih { get; set; }    
+        public bool Aktif {  get; set; }
+        public int Defans { get; set; }
+
+
+        //kurucu metot tanımlama.
+        //Sadece kurucu metotlarda metod adı sınıf ile aynıdır
+        //Kurucu metot tanımlandıktan sonra parameterler yazılır
+        public Oyuncu(string id, string ad, string soyad, string posta, double puan,DateTime kayitTarih,bool aktifmi)
+        {
+            Id = id;
+            Ad = ad;
+            Soyad = soyad;
+            Posta = posta;
+            Puan = puan;
+            KayitTarih = kayitTarih;
+            Aktif = aktifmi;
+
+            DefansArttir();
+        }
+
+        public string BilgiYaz()
+        {
+            return "ID :\t\t" + Id 
+                + "\nAd:\t\t" + Ad
+                + "\nSoyad :\t\t" + Soyad
+                + "\nKayıt Tarihi:\t" + KayitTarih
+                + "\nDefans:" + Defans;
+        }
+
+        public void DefansArttir()
+        {
+            Random random = new Random();
+            Defans = random.Next(10,20);
+        }
+
+ 
+    }
+}
